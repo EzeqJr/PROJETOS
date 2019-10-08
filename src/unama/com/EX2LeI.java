10f4a47237496c38aa9f8da2f6d419d67c8b9051
Ezequias JR <ezequias.silva.9212@gmail.com>
2019-10-07 23:29:16 -0300

    i

---
+++ src/unama/com/EX2LeI.java
@@ -0,0 +1,26 @@
+package unama.com;
+
+import java.util.Scanner;
+
+public class EX2LeI {
+    public  static  int sexo;
+    public static double altura;
+    public static double pesoIdeal;
+    public  static  void  main ( String [] args ) {
+        Scanner sc =  new Scanner ( System .in);
+        System.out.println ( " Para informar seu sexo insira <1> para Masculino ou <2> para Feminino: " );
+        sexo = sc . nextInt ();
+        System.out.println ( " Insira sua altura: " );
+        altura = sc . nextDouble ();
+
+        if (sexo ==  1 ) {
+            pesoIdeal = ( 72.7 * altura) - 58;
+            System.out.println ( " seu peso ideal é: " + pesoIdeal * - 1 );
+        }
+        if (sexo ==  2 ) {
+            pesoIdeal = ( 62.1 * altura) - 44.7 ;
+            System.out.println ( " Seu peso ideal é: " + pesoIdeal * - 1 );
+
+        }
+    }
+}
